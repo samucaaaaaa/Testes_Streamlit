5381b190b5cdb80b8dea8af5117aa66b5a701bd7
samucaaaaaa <mucaspeedpvp@gmail.com>
2023-11-02 16:11:44 -0300

    Adicionando teste

--- primeiro_teste.py
+++ primeiro_teste.py
@@ -1,1 +1,25 @@
 import pandas as pd
+import streamlit as st
+
+st.set_page_config(page_title="Meu Site Streamlit")
+
+
+with st.container():
+    st.subheader("Meu primeiro site com o Streamlit")
+    st.title("Dashboard de Contratos")
+    st.write("Informações sobre os contratos fechados pela Hash&Co ao longo de maio")
+    st.write("Quer acessar o projeto pelo GitHub? [Clique aqui](https://github.com/samucaaaaaa/Testes_Streamlit)")
+
+
+@st.cache_data
+def carregar_dados():
+    tabela = pd.read_csv("resultados.csv")
+    return tabela
+
+with st.container():
+    st.write("---")
+    qtde_dias = st.selectbox("Selecione o período", ["7D", "15D", "21D", "30D"])
+    num_dias = int(qtde_dias.replace("D", ""))
+    dados = carregar_dados()
+    dados = dados[-num_dias:]
+    st.area_chart(dados, x="Data", y="Contratos")
